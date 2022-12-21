d8b1fc7d54a827007beb6b4a0ed2c2cf21a9b443
nedved <nadavleder@gmail.com>
2022-12-21 15:13:24 +0200

    feat: SymbolTable.py.1 finished

--- projects/11/SymbolTable.py
+++ projects/11/SymbolTable.py
@@ -16,15 +16,17 @@ class SymbolTable:
 
     def __init__(self) -> None:
         """Creates a new empty symbol table."""
-        # Your code goes here!
-        pass
+        self._class_symbol_table = dict()
+        self._subroutine_symbol_table = dict()
+        self._identifier_counter = {"STATIC": 0, "FIELD": 0, "ARG": 0, "VAR": 0}
 
     def start_subroutine(self) -> None:
         """Starts a new subroutine scope (i.e., resets the subroutine's 
         symbol table).
         """
-        # Your code goes here!
-        pass
+        self._subroutine_symbol_table.clear()
+        self._identifier_counter["ARG"] = 0
+        self._identifier_counter["VAR"] = 0
 
     def define(self, name: str, type: str, kind: str) -> None:
         """Defines a new identifier of a given name, type and kind and assigns 
@@ -37,8 +39,13 @@ class SymbolTable:
             kind (str): the kind of the new identifier, can be:
             "STATIC", "FIELD", "ARG", "VAR".
         """
-        # Your code goes here!
-        pass
+        if kind in ["STATIC", "FIELD"]:
+            self._class_symbol_table[name] = [type, kind, self._identifier_counter[kind]]
+        elif kind in ["ARG", "VAR"]:
+            self._subroutine_symbol_table[name] = [type, kind, self._identifier_counter[kind]]
+        else:
+            raise TypeError("Kind exception")
+        self._identifier_counter[kind] += 1
 
     def var_count(self, kind: str) -> int:
         """
@@ -49,8 +56,7 @@ class SymbolTable:
             int: the number of variables of the given kind already defined in 
             the current scope.
         """
-        # Your code goes here!
-        pass
+        return self._identifier_counter[kind]
 
     def kind_of(self, name: str) -> str:
         """
@@ -61,8 +67,12 @@ class SymbolTable:
             str: the kind of the named identifier in the current scope, or None
             if the identifier is unknown in the current scope.
         """
-        # Your code goes here!
-        pass
+        if name in self._subroutine_symbol_table:
+            return self._subroutine_symbol_table[name][0]
+        elif name in self._class_symbol_table:
+            return self._class_symbol_table[name][0]
+        else:
+            raise NameError("Kind Of lookup failed :Symbol doesn't exist in scope")
 
     def type_of(self, name: str) -> str:
         """
@@ -72,8 +82,12 @@ class SymbolTable:
         Returns:
             str: the type of the named identifier in the current scope.
         """
-        # Your code goes here!
-        pass
+        if name in self._subroutine_symbol_table:
+            return self._subroutine_symbol_table[name][1]
+        elif name in self._class_symbol_table:
+            return self._class_symbol_table[name][1]
+        else:
+            raise NameError("Type lookup failed :Symbol doesn't exist in scope")
 
     def index_of(self, name: str) -> int:
         """
@@ -83,5 +97,9 @@ class SymbolTable:
         Returns:
             int: the index assigned to the named identifier.
         """
-        # Your code goes here!
-        pass
+        if name in self._subroutine_symbol_table:
+            return self._subroutine_symbol_table[name][2]
+        elif name in self._class_symbol_table:
+            return self._class_symbol_table[name][2]
+        else:
+            raise NameError("Index Of lookup failed :Symbol doesn't exist in scope")
